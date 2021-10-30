20568baebc1be3a9c653ee7c8de6d182087928d2
jchebert23 <clayton.hebert@yale.edu>
2021-10-30 14:54:49 -0700

    basic list view working

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, Text, View, FlatList, Dimensions} from 'react-native';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+const numColumns = 2
+const horizMargin = 16
+const size = ((Dimensions.get('window').width)-(numColumns*2*horizMargin))/numColumns
 
 export default function App() {
+  const renderItem = ({ item }) => (
+    <Item title={item.title}/>
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        numColumns = {numColumns}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 0,
+    marginVertical: 8,
+    marginHorizontal: horizMargin,
+    width: size,
+    height: size
+  },
+  title: {
+    fontSize: 32,
   },
 });
