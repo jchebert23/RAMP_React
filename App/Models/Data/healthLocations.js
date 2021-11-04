f783d693f56640d333dd5c5742dfd681945f038b
jchebert23 <clayton.hebert@yale.edu>
2021-11-03 23:33:40 -0700

    data looks proper, going to try to zoom map in now

---
+++ App/Models/Data/healthLocations.js
@@ -0,0 +1,28 @@
+import MySVGs from '../../../assets/icons';
+
+export default healthLocations = [
+        {
+            "name": "Yale New Haven",
+            "email": "miyah.myles@gmail.com",
+            "locationType": "Hospital",
+            "photo": MySVGs.Health
+        },
+        {
+            "name": "Yale Health",
+            "email": "june.cha@gmail.com",
+            "locationType": "Acute Care",
+            "photo": MySVGs.Health
+        },
+        {
+            "name": "Planned Parenthood",
+            "email": "iida.niskanen@gmail.com",
+            "locationType": "Reproductive Health",
+            "photo": MySVGs.Health
+        },
+        {
+            "name": "New Haven Dental Group",
+            "email": "renee.sims@gmail.com",
+            "locationType": "Dental Care",
+            "photo": MySVGs.Health
+        }
+    ]
