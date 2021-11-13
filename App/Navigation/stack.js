7826a367ceda36d51cb121d994f0513bf152192b
jchebert23 <clayton.hebert@yale.edu>
2021-11-13 12:35:05 -0800

    icon kind of appearing at top now

--- App/Navigation/stack.js
+++ App/Navigation/stack.js
@@ -2,18 +2,28 @@ import HomeScreen  from "../Views/homeScreen"
 import MapScreen from "../Views/mapScreen"
 import GuideScreen from "../Views/guideScreen";
 import React from 'react';
+import { Text, View, FlatList, Image, TouchableOpacity, useWindowDimensions, Dimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { getNavigationStyles } from "../Views/styles";
 
 const Stack = createNativeStackNavigator();
-
+const styles = getNavigationStyles()
 function NavigationTool() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} options={{title: 'Resource Access Mapping Project'}} />
         <Stack.Screen name="Map" component={MapScreen} options={{title: 'Health Resources'}} />
-        <Stack.Screen name="Guide" component={GuideScreen} options={{title: ''}} />
+        <Stack.Screen name="Guide" component={GuideScreen} 
+        options={({route})=>
+          ({
+            headerTitle: () =>   
+            <View style={styles.iconAtTopOfScreen}>
+                <route.params.resource.icon width="78%" height="78%"/>
+            </View>
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
