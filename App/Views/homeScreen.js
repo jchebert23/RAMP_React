cc514094b9ac326f34bf9728a597f17a88fa4d0c
jchebert23 <clayton.hebert@yale.edu>
2022-04-05 20:58:21 -0700

    disabled all columns besides food

--- App/Views/homeScreen.js
+++ App/Views/homeScreen.js
@@ -10,7 +10,7 @@ const styles = getHomeScreenStyles(2)
 function Item({ item, navigation}) {
     const ResourceIcon = getResourceIcon(item.id)
     return (
-      <TouchableOpacity style={styles.listItem} onPress={()=> navigation.push('Guide',{id: item.id, groupId: item.groupId})} >
+      <TouchableOpacity disabled={item.disabled} style={styles.listItem} onPress={()=> navigation.push('Guide',{id: item.id, groupId: item.groupId})} >
         <ResourceIcon width="78%" height="78%"/>
       </TouchableOpacity>
     );
